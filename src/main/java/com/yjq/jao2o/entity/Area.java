b5f036df8537fa49146a66dabe92b3007eff32db
Aurel1ano <875647816@qq.com>
2020-08-24 01:16:39 +0800

    修改area

--- src/main/java/com/yjq/jao2o/entity/Area.java
+++ src/main/java/com/yjq/jao2o/entity/Area.java
@@ -1,29 +1,24 @@
 package com.yjq.jao2o.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import java.util.Date;
 
+@Data
 public class Area {
 
     // 区域Id
-    @Getter @Setter
     private Integer areaId;
 
     // 区域名称
-    @Getter @Setter
     private String areaName;
 
     // 权重
-    @Getter @Setter
     private Integer priority;
 
     // 创建时间
-    @Getter @Setter
     private Date createTime;
 
     //最后编辑时间
-    @Getter @Setter
     private Date lastEditTime;
 
 }
